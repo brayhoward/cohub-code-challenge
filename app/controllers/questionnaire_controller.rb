d7f1d1efa151e90c9fc4d5f75d454447db3e82fb
brayhoward <brandonrhoward@gmail.com>
2018-05-20 23:31:14 -0500

    Make auth session expire after one min.

--- app/controllers/questionnaire_controller.rb
+++ app/controllers/questionnaire_controller.rb
@@ -3,7 +3,7 @@ class QuestionnaireController < ApplicationController
 
   def authenticate
     if params[:password] === "veryhardpassword"
-      cookies[:auth_key] = @auth_key
+      cookies[:auth_key] = { value: @auth_key, expires: Time.zone.now + 1.minute }
       head :ok
     else
       head :forbidden
